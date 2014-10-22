6c11734661a90126eca3a931fe2dbd4eca187e90
Fabian Schmid <fs@studer-raimann.ch>
2014-10-22 10:57:10 +0200

    Prepare release

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id                = 'xlvo';
 $version           = '1.1.01';
-$ilias_min_version = '4.2.0';
-$ilias_max_version = '4.5.999';
+$ilias_min_version = '4.3.0';
+$ilias_max_version = '4.4.999';
 $responsible       = 'Oskar Truffer, Fabian Schmid';
 $responsible_mail  = 'info at studer-raimann.ch';
 ?>
