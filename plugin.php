5adce05aeb9ebd906804d70ba0a19c7efd2b2461
Michael Herren <michherren@gmail.com>
2015-02-25 11:28:43 +0100

    Updated ilias_max_version to 5.1

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'ctrlmm';
 $version = '3.1.00';
 $ilias_min_version = '4.4.000';
-$ilias_max_version = '5.0.999';
+$ilias_max_version = '5.1.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
 ?>
