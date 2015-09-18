f0e2a0f8644efc00437f9a5d80ce483266eae813
Daniel Aemmer <daniel.aemmer@phbern.ch>
2015-09-18 15:27:39 +0200

    access check for save sorting

--- classes/voting/class.xlvoVotingGUI.php
+++ classes/voting/class.xlvoVotingGUI.php
@@ -392,16 +392,20 @@ class xlvoVotingGUI {
 
 
 	protected function saveSorting() {
-		foreach ($_POST['position'] as $k => $v) {
-			/**
-			 * @var $xlvoVoting xlvoVoting
-			 */
-			$xlvoVoting = xlvoVoting::find($v);
-			$xlvoVoting->setPosition($k + 1);
-			$xlvoVoting->update();
+		if (! $this->access->hasWriteAccess()) {
+			ilUtil::sendFailure($this->pl->txt('permission_denied'), true);
+		} else {
+			foreach ($_POST['position'] as $k => $v) {
+				/**
+				 * @var $xlvoVoting xlvoVoting
+				 */
+				$xlvoVoting = xlvoVoting::find($v);
+				$xlvoVoting->setPosition($k + 1);
+				$xlvoVoting->update();
+			}
+			ilUtil::sendSuccess($this->pl->txt('sorting_saved'), true);
+			$this->ctrl->redirect($this, self::CMD_STANDARD);
 		}
-		ilUtil::sendSuccess($this->pl->txt('sorting_saved'), true);
-		$this->ctrl->redirect($this, self::CMD_STANDARD);
 	}
 
 
