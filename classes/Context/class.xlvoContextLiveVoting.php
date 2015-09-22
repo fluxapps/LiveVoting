bd5c5bc13ca97e5cfad1ccb9b43fde42cb26c05a
Daniel Aemmer <daniel.aemmer@phbern.ch>
2015-09-22 16:15:27 +0200

    context added again

---
+++ classes/Context/class.xlvoContextLiveVoting.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once('Services/Context/classes/class.ilContextBase.php');
+
+/**
+ * Class xlvoContextLiveVoting
+ *
+ * @author Fabian Schmid <fs@studer-raimann.ch>
+ *
+ */
+class xlvoContextLiveVoting extends ilContextBase {
+
+	/**
+	 * @return bool
+	 */
+	public static function supportsRedirects() {
+		return false;
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public static function hasUser() {
+		return true;
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public static function usesHTTP() {
+		return false;
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public static function hasHTML() {
+		return true;
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public static function usesTemplate() {
+		return true;
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public static function initClient() {
+		return true;
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public static function doAuthentication() {
+		return false;
+	}
+}
