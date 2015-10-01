e3340a87487af996326008487ea62663e8e60e7f
Fabian Schmid <fs@studer-raimann.ch>
2015-10-01 08:42:32 +0200

    removed archive

--- archive/plugin.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id = 'xlvo';
-$version = '2.0.2';
-$ilias_min_version = '4.4.0';
-$ilias_max_version = '5.0.999';
-$responsible = 'Oskar Truffer, Fabian Schmid';
-$responsible_mail = 'info at studer-raimann.ch';
-?>
