a72e19f57baf17a6bda5b8bd93988fb9e17761d2
gc <gc@studer-raimann.ch>
2016-03-16 10:57:46 +0100

    [FIX] Abstract class "ilContextBase" has been replaced by interface "ilContextTemplate" in ILIAS 5.2

--- classes/Context/class.xlvoContextLiveVoting.php
+++ classes/Context/class.xlvoContextLiveVoting.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once('Services/Context/classes/class.ilContextBase.php');
+// TODO SRAG-GC IMPLEMENT INTERFACE - EXPLINATION: In ILIAS 5.2 the abstract class ilContextBase has been replaced by the interface ilContextTemplate.
+// TODO SRAG-GC IMPLEMENT INTERFACE - EXPLINATION: In order to stay compatible as well with ILIAS < 5.2 and ILIAS >= 5.2, we removed as well "extends ilContextBase" as "implement ilContextTemplate"
+// TODO SRAG-GC IMPLEMENT INTERFACE - EXPLINATION: However, as soon as this plugin won't support ILIAS 5.1 anymore, we should implement the interface again
+
+// TODO SRAG-GC IMPLEMENT INTERFACE - STEP 1: as soon as this plugin does not support ILIAS 5.1 anymore, uncomment the following line
+//require_once('Services/Context/interfaces/interface.ilContextTemplate.php');
 
 /**
  * Class xlvoContextLiveVoting
@@ -8,7 +13,9 @@ require_once('Services/Context/classes/class.ilContextBase.php');
  * @author Fabian Schmid <fs@studer-raimann.ch>
  *
  */
-class xlvoContextLiveVoting extends ilContextBase {
+// TODO SRAG-GC IMPLEMENT INTERFACE - STEP 2: as soon as this plugin does not support ILIAS 5.1 anymore, uncomment the following line, and remove the after next line
+//class xlvoContextLiveVoting implements ilContextTemplate {
+class xlvoContextLiveVoting {
 
 	/**
 	 * @return bool
