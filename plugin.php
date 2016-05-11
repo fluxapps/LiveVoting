db0d82ac75d6745a67fd3b64ce1fdcf061df4cd1
Fabian Schmid <fs@studer-raimann.ch>
2016-05-11 13:56:57 +0200

    changed plugin-version

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'xlvo';
-$version = '3.0.2';
+$version = '3.0.3';
 $ilias_min_version = '5.0.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.1.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'info at studer-raimann.ch';
