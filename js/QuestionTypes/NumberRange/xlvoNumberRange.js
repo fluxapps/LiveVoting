815683dbbc601e20e3ed1fef883a2ac06bc56a1b
Nicolas Schäfli <ns@studer-raimann.ch>
2017-06-13 17:39:50 +0200

    Added arrow buttons to range question voter view.

--- js/QuestionTypes/NumberRange/xlvoNumberRange.js
+++ js/QuestionTypes/NumberRange/xlvoNumberRange.js
@@ -30,6 +30,26 @@ var xlvoNumberRange = {
 			function (changedValues) {
 				$('#number-display').text(String(changedValues.value.newValue).concat(thisRef.percentageSign));
 		}).bind(thisRef);
+
+		//left button click event to move the slider to the left
+		var buttonMoveSliderLeft = document.querySelector("#btn-slider-left");
+		buttonMoveSliderLeft.onclick = function() {
+
+			var sliderValue = slider.bootstrapSlider("getValue");
+			if(slider.bootstrapSlider("getAttribute", "min") < sliderValue)
+				slider.bootstrapSlider("setValue", sliderValue - 1, false, true);
+
+		}.bind(slider);
+
+		//right button click event to move the slider to the right
+		var buttonMoveSliderRight = document.querySelector("#btn-slider-right");
+		buttonMoveSliderRight.onclick = function() {
+
+			var sliderValue = slider.bootstrapSlider("getValue");
+			if(slider.bootstrapSlider("getAttribute", "max") > sliderValue)
+				slider.bootstrapSlider("setValue", sliderValue + 1, false, true);
+
+		}.bind(slider);
 	},
 	/**
 	 * @param button_id
