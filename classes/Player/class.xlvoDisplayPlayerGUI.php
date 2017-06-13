18dd1724e2bc5ef199299148b4deac1e3a2c58bb
Nicolas Schäfli <ns@studer-raimann.ch>
2017-06-13 16:18:16 +0200

    The orderable questions now are also randomized on the presenter view.

--- classes/Player/class.xlvoDisplayPlayerGUI.php
+++ classes/Player/class.xlvoDisplayPlayerGUI.php
@@ -66,6 +66,10 @@ class xlvoDisplayPlayerGUI {
 		else {
 			//add options to player
 			$xlvoOptions = $this->manager->getVoting()->getVotingOptions();
+			usort($xlvoOptions, function ($option1, $option2) {
+				return $option1->getPosition() < $option2->getPosition();
+			});
+
 			foreach ($xlvoOptions as $item) {
 				$this->addOption($item);
 			}
--- classes/QuestionTypes/CorrectOrder/class.xlvoCorrectOrderSubFormGUI.php
+++ classes/QuestionTypes/CorrectOrder/class.xlvoCorrectOrderSubFormGUI.php
@@ -22,8 +22,8 @@ class xlvoCorrectOrderSubFormGUI extends xlvoSubFormGUI {
 
 	protected function initFormElements() {
 		$xlvoMultiLineInputGUI = new xlvoMultiLineInputGUI($this->txt(self::F_OPTIONS), self::F_OPTIONS);
+		$xlvoMultiLineInputGUI->setPositionMovable(false);
 		$xlvoMultiLineInputGUI->setShowLabel(true);
-		$xlvoMultiLineInputGUI->setPositionMovable(true);
 
 		$h = new \ilHiddenInputGUI(self::F_ID);
 		$xlvoMultiLineInputGUI->addInput($h);
@@ -108,8 +108,79 @@ class xlvoCorrectOrderSubFormGUI extends xlvoSubFormGUI {
 				$xlvoOption->delete();
 			}
 		}
+
+		//randomize the order on save
+		$this->randomiseOptionPosition($this->options);
+		foreach ($this->options as $option) {
+			$option->update();
+		}
+
 		$this->getXlvoVoting()->setMultiFreeInput(true);
-		$this->getXlvoVoting()->renegerateOptionSorting();
 		$this->getXlvoVoting()->update();
+	}
+
+
+	/**
+	 * Randomises the position of the given options the position in the array is not modified at all.
+	 *
+	 * @param xlvoOption[] $options The options which should be randomised.
+	 *
+	 * @return void
+	 */
+	private function randomiseOptionPosition(array &$options) {
+
+		//reorder only if there is something to reorder
+		if(count($options) < 1)
+			return;
+
+		$optionsLength = count($options);
+		foreach ($options as $option) {
+			$newPosition = rand(1, $optionsLength);
+			$previousOption = $this->findOptionByPosition($options, $newPosition);
+			$previousOption->setPosition($option->getPosition());
+			$option->setPosition($newPosition);
+		}
+
+		//check if we got the correct result
+		if($this->isNotCorrectlyOrdered($options))
+			return;
+
+		//we got the right result reshuffle
+		$this->randomiseOptionPosition($options);
+	}
+
+
+	/**
+	 * Searches an option within the given option array by position.
+	 *
+	 * @param xlvoOption[]  $options    The options which should be used to search the position.
+	 * @param int           $position   The position which should be searched for.
+	 *
+	 * @return xlvoOption
+	 * @throws InvalidArgumentException Thrown if the position is not found within the given options.
+	 */
+	private function findOptionByPosition(array &$options, $position) {
+		foreach ($options as $option) {
+			if($option->getPosition() === $position)
+				return $option;
+		}
+
+		throw new InvalidArgumentException("Supplied position \"$position\" can't be found within the given options.");
+	}
+
+	/**
+	 * Checks if at least one element is not correctly ordered.
+	 *
+	 * @param xlvoOption[] $options     The options which should be checked.
+	 * @return bool                     True if at least one element is not correctly ordered otherwise false.
+	 */
+	private function isNotCorrectlyOrdered(array &$options) {
+		$incorrectOrder = 0;
+		foreach ($options as $option) {
+			if(strcmp($option->getCorrectPosition(), strval($option->getPosition())) !== 0)
+				$incorrectOrder++;
+		}
+
+		return $incorrectOrder > 0;
 	}
 }
