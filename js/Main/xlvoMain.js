c2213ea5d034b0b72e15237235c90490a4f55772
Fabian Schmid <fs@studer-raimann.ch>
2018-01-22 15:06:47 +0100

    fixed deprecated js method

--- js/Main/xlvoMain.js
+++ js/Main/xlvoMain.js
@@ -4,14 +4,14 @@
  */
 var xlvoMain = {
     init: function () {
-        var $subtabSubtabEdit = $('#subtab_subtab_edit a');
-        $subtabSubtabEdit.disableSelection();
-        $subtabSubtabEdit.click(function (evt) {
-            if (evt.shiftKey) {
-                window.location.href = $subtabSubtabEdit.attr('href') + '&import=1';
-                return false;
-            }
-            return true;
-        });
+        // var $subtabSubtabEdit = $('#subtab_subtab_edit a');
+        // $subtabSubtabEdit.disableSelection();
+        // $subtabSubtabEdit.click(function (evt) {
+        //     if (evt.shiftKey) {
+        //         window.location.href = $subtabSubtabEdit.attr('href') + '&import=1';
+        //         return false;
+        //     }
+        //     return true;
+        // });
     }
 };
