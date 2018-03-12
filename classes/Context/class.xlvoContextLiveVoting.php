7854dba28ed938996829de8446f10987fe182b3a
Florian <fw@studer-raimann.ch>
2018-03-12 13:56:41 +0100

    Use ilContextTemplate

--- classes/Context/class.xlvoContextLiveVoting.php
+++ classes/Context/class.xlvoContextLiveVoting.php
@@ -2,12 +2,7 @@
 
 namespace LiveVoting\Context;
 
-// TODO SRAG-GC IMPLEMENT INTERFACE - EXPLINATION: In ILIAS 5.2 the abstract class ilContextBase has been replaced by the interface ilContextTemplate.
-// TODO SRAG-GC IMPLEMENT INTERFACE - EXPLINATION: In order to stay compatible as well with ILIAS < 5.2 and ILIAS >= 5.2, we removed as well "extends ilContextBase" as "implement ilContextTemplate"
-// TODO SRAG-GC IMPLEMENT INTERFACE - EXPLINATION: However, as soon as this plugin won't support ILIAS 5.1 anymore, we should implement the interface again
-
-// TODO SRAG-GC IMPLEMENT INTERFACE - STEP 1: as soon as this plugin does not support ILIAS 5.1 anymore, uncomment the following line
-//require_once('Services/Context/interfaces/interface.ilContextTemplate.php');
+require_once('Services/Context/interfaces/interface.ilContextTemplate.php');
 
 /**
  * Class xlvoContextLiveVoting
@@ -15,9 +10,7 @@ namespace LiveVoting\Context;
  * @author Fabian Schmid <fs@studer-raimann.ch>
  *
  */
-// TODO SRAG-GC IMPLEMENT INTERFACE - STEP 2: as soon as this plugin does not support ILIAS 5.1 anymore, uncomment the following line, and remove the after next line
-//class xlvoContextLiveVoting implements ilContextTemplate {
-class xlvoContextLiveVoting {
+class xlvoContextLiveVoting implements \ilContextTemplate {
 
 	/**
 	 * @return bool
@@ -71,6 +64,25 @@ class xlvoContextLiveVoting {
 	 * @return bool
 	 */
 	public static function doAuthentication() {
+		return false;
+	}
+
+
+	/**
+	 * Check if persistent sessions are supported
+	 * false for context cli
+	 */
+	public static function supportsPersistentSessions() {
+		return false;
+	}
+
+
+	/**
+	 * Check if push messages are supported, see #0018206
+	 *
+	 * @return bool
+	 */
+	public static function supportsPushMessages() {
 		return false;
 	}
 }
