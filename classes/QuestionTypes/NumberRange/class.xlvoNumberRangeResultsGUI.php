54bb3472fa4e4b7232b02fb41ebc2dce441b7628
Fabian Schmid <fs@studer-raimann.ch>
2018-03-20 10:54:10 +0100

    reiplemented feature/PLLV-194/presentation-of-number-range

--- classes/QuestionTypes/NumberRange/class.xlvoNumberRangeResultsGUI.php
+++ classes/QuestionTypes/NumberRange/class.xlvoNumberRangeResultsGUI.php
@@ -1,4 +1,5 @@
 <?php
+
 use LiveVoting\Vote\xlvoVote;
 
 /**
@@ -6,17 +7,25 @@ use LiveVoting\Vote\xlvoVote;
  *
  * @author  Nicolas Schäfli <ns@studer-raimann.ch>
  */
-class xlvoNumberRangeResultsGUI extends xlvoInputResultsGUI{
+class xlvoNumberRangeResultsGUI extends xlvoInputResultsGUI {
 
 	const BAR_COUNT = 5;
-	
+
+	const DISPLAY_MODE_GROUPED_TEXT = 0;
+	const DISPLAY_MODE_BARS = 1;
+	const DISPLAY_MODE_GROUPED_TEXT_EXTENDED = 2;
+
+
 	public function getHTML() {
-
-		if($this->voting->getAltResultDisplayMode() === 1) {
-			return $this->renderBarResult();
+		switch ($this->voting->getAltResultDisplayMode()) {
+			case self::DISPLAY_MODE_BARS:
+				return $this->renderBarResult();
+			case self::DISPLAY_MODE_GROUPED_TEXT_EXTENDED:
+				return $this->renderGroupedTextResultWithInfo();
+			case self::DISPLAY_MODE_GROUPED_TEXT:
+			default:
+				return $this->renderGroupedTextResult();
 		}
-
-		return $this->renderGroupedTextResult();
 	}
 
 
@@ -44,18 +53,84 @@ class xlvoNumberRangeResultsGUI extends xlvoInputResultsGUI{
 
 
 	/**
+	 * @return string
+	 */
+	private function renderGroupedTextResultWithInfo() {
+		$votes = $this->manager->getVotesOfVoting();
+		$vote_count = $this->manager->countVotes();
+
+		$vote_sum = 0;
+		$values = [];
+		$modes = [];
+
+		array_walk($votes, function (xlvoVote $vote) use (&$vote_sum, &$values, &$modes) {
+			$value = (int)$vote->getFreeInput();
+			$values[] = $value;
+			$modes[$value]++;
+			$vote_sum = $vote_sum + $value;
+		});
+		$relevant_modes = [];
+		foreach ($modes as $given_value => $counter) {
+			if ($counter == max($modes)) {
+				$relevant_modes[] = $given_value;
+			}
+		}
+
+		$calculateMedian = function ($aValues) {
+			$aToCareAbout = array();
+			foreach ($aValues as $mValue) {
+				if ($mValue >= 0) {
+					$aToCareAbout[] = $mValue;
+				}
+			}
+			$iCount = count($aToCareAbout);
+			sort($aToCareAbout, SORT_NUMERIC);
+			if ($iCount > 2) {
+				if ($iCount % 2 == 0) {
+					return ($aToCareAbout[floor($iCount / 2) - 1] + $aToCareAbout[floor($iCount / 2)]) / 2;
+				} else {
+					return $aToCareAbout[$iCount / 2];
+				}
+			} elseif (isset($aToCareAbout[0])) {
+				return $aToCareAbout[0];
+			} else {
+				return 0;
+			}
+		};
+
+		$info = new xlvoBarCollectionGUI();
+		$mean = new xlvoBarInfoGUI('Mean', round($vote_sum / $vote_count, 2));
+		$mean->setBig(true);
+		$mean->setDark(true);
+		$mean->setCenter(true);
+		$info->addBar($mean);
+
+		$median = new xlvoBarInfoGUI('Median', $calculateMedian($values));
+		$median->setBig(true);
+		$median->setCenter(true);
+		$median->setDark(true);
+		$info->addBar($median);
+
+		$mode = new xlvoBarInfoGUI('Mode', implode(',', $relevant_modes));
+		$mode->setBig(true);
+		$mode->setDark(true);
+		$mode->setCenter(true);
+		$info->addBar($mode);
+
+		return $info->getHTML() . "<div class='row'><br></div>" . $this->renderGroupedTextResult();
+	}
+
+
+	/**
 	 * Render a result page which shows all answers as text.
 	 * The answers are grouped together and sorted descending.
 	 *
 	 * @return string The rendered result page.
 	 */
 	private function renderGroupedTextResult() {
-
-		$votes = $this->manager->getVotesOfVoting();
-
 		$bars = new xlvoBarGroupingCollectionGUI();
 		$bars->sorted(true);
-
+		$votes = $this->manager->getVotesOfVoting();
 		foreach ($votes as $value) {
 			$bar = new xlvoBarFreeInputsGUI($this->voting, $value);
 			$bar->setBig(true);
@@ -76,8 +151,10 @@ class xlvoNumberRangeResultsGUI extends xlvoInputResultsGUI{
 	 */
 	public function getTextRepresentationForVotes($votes) {
 		$result = xlvoNumberRangeResultGUI::getInstance($this->voting);
+
 		return $result->getTextRepresentation($votes);
 	}
+
 
 	/**
 	 * Fetches all data and simplifies them to an array with 10 values.
@@ -86,8 +163,7 @@ class xlvoNumberRangeResultsGUI extends xlvoInputResultsGUI{
 	 *
 	 * @return string[]
 	 */
-	private function getAllVoteValues()
-	{
+	private function getAllVoteValues() {
 		$percentage = ((int)$this->manager->getVoting()->getPercentage() === 1) ? '%' : '';
 
 		//generate array which is equal in its length to the range from start to end
@@ -102,8 +178,7 @@ class xlvoNumberRangeResultsGUI extends xlvoInputResultsGUI{
 		/**
 		 * @var xlvoVote $vote
 		 */
-		foreach ($votes as $vote)
-		{
+		foreach ($votes as $vote) {
 			$value = (int)$vote->getFreeInput();
 			$values[$value]++;
 		}
@@ -112,8 +187,7 @@ class xlvoNumberRangeResultsGUI extends xlvoInputResultsGUI{
 		$slices = [];
 		$sliceWidth = ceil($count / self::BAR_COUNT);
 
-		for($i = 0; $i < $count; $i += $sliceWidth)
-		{
+		for ($i = 0; $i < $count; $i += $sliceWidth) {
 			//create a slice
 			$slice = array_slice($values, $i, $sliceWidth, true);
 
@@ -125,15 +199,15 @@ class xlvoNumberRangeResultsGUI extends xlvoInputResultsGUI{
 			$keyCount = count($keys);
 
 			//only display a range if we got more than one element
-			if($keyCount > 1)
+			if ($keyCount > 1) {
 				$key = "{$keys[0]}$percentage - {$keys[$keyCount - 1]}$percentage";
-			else
+			} else {
 				$key = "{$keys[0]}$percentage";
+			}
 
 			//create now slice entry
 			$slices[$key] = $sum;
 		}
-
 
 		return $slices;
 	}
--- classes/QuestionTypes/NumberRange/class.xlvoNumberRangeSubFormGUI.php
+++ classes/QuestionTypes/NumberRange/class.xlvoNumberRangeSubFormGUI.php
@@ -8,26 +8,20 @@
  *
  * @author  Nicolas Schäfli <ns@studer-raimann.ch>
  */
-class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI{
+class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI {
 
 	const OPTION_PERCENTAGE = 'option_percentage';
 	const OPTION_PERCENTAGE_INFO = 'option_percentage_info';
-
 	const OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE = 'option_alternative_result_display_mode';
 	const OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE_INFO = 'option_alternative_result_display_mode_info';
-
 	const OPTION_RANGE_START = 'option_range_start';
 	const OPTION_RANGE_START_INFO = 'option_range_start_info';
-
 	const OPTION_RANGE_END = 'option_range_end';
 	const OPTION_RANGE_END_INFO = 'option_range_end_info';
-
 	const START_RANGE_INVALID_INFO = 'qtype_6_invalid_start_range';
 	const END_RANGE_INVALID_INFO = 'qtype_6_invalid_end_range';
-
 	const END_RANGE_MAX = 2147483646;  //2^31 - 1       (int is always signed in php)
 	const END_RANGE_MIN = -2147483647; //-2^31 + 1      (int is always signed in php)
-	
 	const START_RANGE_MAX = 2147483645; //2^31 - 2      (int is always signed in php)
 	const START_RANGE_MIN = -2147483646; //-2^31 + 2    (int is always signed in php)
 
@@ -43,9 +37,20 @@ class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI{
 		$percentageCheckBox->setChecked(((int)$this->getXlvoVoting()->getStartRange()) === 1);
 
 		//create badge box option for the result display
-		$alternativeResultDisplayModeCheckBox = new \ilCheckboxInputGUI($this->txt(self::OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE), self::OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE);
+		$alternativeResultDisplayModeCheckBox = new \ilCheckboxInputGUI($this->txt(self::OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE),
+			self::OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE);
 		$alternativeResultDisplayModeCheckBox->setInfo($this->txt(self::OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE_INFO));
 		$alternativeResultDisplayModeCheckBox->setChecked(((int)$this->getXlvoVoting()->getAltResultDisplayMode()) === 1);
+
+		$displayMode = new ilRadioGroupInputGUI($this->txt(self::OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE),
+			self::OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE);
+		$displayMode->addOption(new ilRadioOption($this->txt('display_mode_nr_' . xlvoNumberRangeResultsGUI::DISPLAY_MODE_GROUPED_TEXT),
+			xlvoNumberRangeResultsGUI::DISPLAY_MODE_GROUPED_TEXT));
+		$displayMode->addOption(new ilRadioOption($this->txt('display_mode_nr_' . xlvoNumberRangeResultsGUI::DISPLAY_MODE_BARS),
+			xlvoNumberRangeResultsGUI::DISPLAY_MODE_BARS));
+		$displayMode->addOption(new ilRadioOption($this->txt('display_mode_nr_' . xlvoNumberRangeResultsGUI::DISPLAY_MODE_GROUPED_TEXT_EXTENDED),
+			xlvoNumberRangeResultsGUI::DISPLAY_MODE_GROUPED_TEXT_EXTENDED));
+		$displayMode->setValue($this->getXlvoVoting()->getAltResultDisplayMode());
 
 		//create start range number input
 		$startRange = new ilNumberInputGUI($this->txt(self::OPTION_RANGE_START), self::OPTION_RANGE_START);
@@ -64,7 +69,8 @@ class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI{
 
 		//add elements to gui
 		$this->addFormElement($percentageCheckBox);
-		$this->addFormElement($alternativeResultDisplayModeCheckBox);
+		$this->addFormElement($displayMode);
+		//		$this->addFormElement($alternativeResultDisplayModeCheckBox);
 		$this->addFormElement($startRange);
 		$this->addFormElement($endRange);
 	}
@@ -74,17 +80,16 @@ class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI{
 	 * Set a new value by element.
 	 *
 	 * @param ilFormPropertyGUI $element
-	 * @param int               $value      The new value for the element. (value will be casted to int)
+	 * @param int               $value The new value for the element. (value will be casted to int)
 	 *
 	 * @return \LiveVoting\Voting\xlvoVoting
 	 * @throws ilException  If the element is not recognised by the handle field.
 	 */
 	protected function handleField(\ilFormPropertyGUI $element, $value) {
 		$postKey = $element->getPostVar();
-		$value = (int) $value;
+		$value = (int)$value;
 
-		switch ($postKey)
-		{
+		switch ($postKey) {
 			case self::OPTION_PERCENTAGE:
 				return $this->getXlvoVoting()->setPercentage($value === 1 ? 1 : 0); //if the value is 1 set 1 or else 0.
 			case self::OPTION_RANGE_START:
@@ -92,7 +97,7 @@ class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI{
 			case self::OPTION_RANGE_END:
 				return $this->setEndRange($value);
 			case self::OPTION_ALTERNATIVE_RESULT_DISPLAY_MODE:
-				return $this->getXlvoVoting()->setAltResultDisplayMode($value === 1 ? 1 : 0); //if the value is 1 set 1 or else 0.
+				return $this->getXlvoVoting()->setAltResultDisplayMode((int)$value); //if the value is 1 set 1 or else 0.
 			default:
 				throw new ilException('Unknown element can not set the value.');
 		}
@@ -102,7 +107,7 @@ class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI{
 	/**
 	 * Get a value by element.
 	 *
-	 * @param ilFormPropertyGUI $element    The element which should be used to fetch the value.
+	 * @param ilFormPropertyGUI $element The element which should be used to fetch the value.
 	 *
 	 * @return int                          The value read with the help of the given element.
 	 * @throws ilException                  Thrown if the element is not recognised.
@@ -110,8 +115,7 @@ class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI{
 	protected function getFieldValue(\ilFormPropertyGUI $element) {
 		$postKey = $element->getPostVar();
 
-		switch ($postKey)
-		{
+		switch ($postKey) {
 			case self::OPTION_PERCENTAGE:
 				return (int)$this->getXlvoVoting()->getPercentage();
 			case self::OPTION_RANGE_START:
@@ -129,20 +133,19 @@ class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI{
 	/**
 	 * Validates the start of the range and sets the value if valid.
 	 *
-	 * @param int $start    The new start range which should be set.
+	 * @param int $start The new start range which should be set.
 	 *
 	 * @return \LiveVoting\Voting\xlvoVoting
 	 */
-	private function setStartRange($start)
-	{
+	private function setStartRange($start) {
 		$end = (int)$this->getXlvoVoting()->getEndRange();
 
-		if($start < $end && $start <= self::START_RANGE_MAX && $start >= self::START_RANGE_MIN)
-		{
+		if ($start < $end && $start <= self::START_RANGE_MAX && $start >= self::START_RANGE_MIN) {
 			return $this->getXlvoVoting()->setStartRange($start);
 		}
 
 		ilUtil::sendFailure($this->pl->txt(self::START_RANGE_INVALID_INFO));
+
 		return $this->getXlvoVoting();
 	}
 
@@ -150,20 +153,19 @@ class xlvoNumberRangeSubFormGUI extends xlvoSubFormGUI{
 	/**
 	 * Validates the end of the range and sets the value if valid.
 	 *
-	 * @param int $end  The new end range which should be set.
+	 * @param int $end The new end range which should be set.
 	 *
 	 * @return \LiveVoting\Voting\xlvoVoting
 	 */
-	private function setEndRange($end)
-	{
+	private function setEndRange($end) {
 		$start = (int)$this->getXlvoVoting()->getStartRange();
 
-		if($end > $start && $end <= self::END_RANGE_MAX && $end >= self::END_RANGE_MIN)
-		{
+		if ($end > $start && $end <= self::END_RANGE_MAX && $end >= self::END_RANGE_MIN) {
 			return $this->getXlvoVoting()->setEndRange($end);
 		}
 
 		ilUtil::sendFailure($this->pl->txt(self::END_RANGE_INVALID_INFO));
+
 		return $this->getXlvoVoting();
 	}
 }
