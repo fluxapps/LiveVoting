b15ffed53e585e34182044bfd89de84844176664
Florian <fw@studer-raimann.ch>
2018-05-28 10:23:07 +0200

    Plugin uninstaller

--- classes/class.ilLiveVotingPlugin.php
+++ classes/class.ilLiveVotingPlugin.php
@@ -32,6 +32,24 @@ class ilLiveVotingPlugin extends \ilRepositoryObjectPlugin {
 
 
 	/**
+	 * @var ilDB
+	 */
+	protected $db;
+
+
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct();
+
+		global $DIC;
+
+		$this->db = $DIC->database();
+	}
+
+
+	/**
 	 * @return string
 	 */
 	public function getPluginName() {
@@ -39,45 +57,23 @@ class ilLiveVotingPlugin extends \ilRepositoryObjectPlugin {
 	}
 
 
+	/**
+	 * @return bool
+	 */
 	protected function uninstallCustom() {
-		$tables = array(
-			\LiveVoting\Conf\xlvoConfOld::TABLE_NAME,
-			xlvoVotingConfig::TABLE_NAME,
-			\LiveVoting\Option\xlvoData::TABLE_NAME,
-			\LiveVoting\Option\xlvoData::TABLE_NAME . "_seq",
-			\LiveVoting\Option\xlvoOption::TABLE_NAME,
-			\LiveVoting\Option\xlvoOption::TABLE_NAME . "_seq",
-			\LiveVoting\Option\xlvoOptionOld::TABLE_NAME,
-			\LiveVoting\Option\xlvoOptionOld::TABLE_NAME . "_seq",
-			\LiveVoting\Player\xlvoPlayer::TABLE_NAME,
-			\LiveVoting\Player\xlvoPlayer::TABLE_NAME . "_seq",
-			\LiveVoting\Round\xlvoRound::TABLE_NAME,
-			\LiveVoting\Round\xlvoRound::TABLE_NAME . "_seq",
-			\LiveVoting\Vote\xlvoVote::TABLE_NAME,
-			\LiveVoting\Vote\xlvoVote::TABLE_NAME . "_seq",
-			\LiveVoting\Vote\xlvoVoteOld::TABLE_NAME,
-			\LiveVoting\Vote\xlvoVoteOld::TABLE_NAME . "_seq",
-			\LiveVoting\User\xlvoVoteHistoryObject::TABLE_NAME,
-			\LiveVoting\User\xlvoVoteHistoryObject::TABLE_NAME . "_seq",
-			\LiveVoting\Voting\xlvoVoting::TABLE_NAME,
-			\LiveVoting\Voting\xlvoVoting::TABLE_NAME . "_seq",
-			\LiveVoting\Conf\xlvoConf::TABLE_NAME,
-			\LiveVoting\Voter\xlvoVoter::TABLE_NAME,
-			\LiveVoting\Voter\xlvoVoter::TABLE_NAME . "_seq",
-		);
-		global $DIC;
-		$ilDB = $DIC->database();
-		foreach ($tables as $table) {
-			$substr = substr($table, - 4);
-			if ($substr == '_seq') {
-				$table_name_from_sequence = substr($table, 0, - 4);
-				if ($ilDB->sequenceExists($table_name_from_sequence)) {
-					$ilDB->dropSequence($table_name_from_sequence, false);
-				}
-			} else {
-				$ilDB->dropTable($table, false);
-			}
-		}
+		$this->db->dropTable(\LiveVoting\Conf\xlvoConfOld::TABLE_NAME, false);
+		$this->db->dropTable(xlvoVotingConfig::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Option\xlvoData::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Option\xlvoOption::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Option\xlvoOptionOld::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Player\xlvoPlayer::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Round\xlvoRound::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Vote\xlvoVote::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Vote\xlvoVoteOld::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\User\xlvoVoteHistoryObject::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Voting\xlvoVoting::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Conf\xlvoConf::TABLE_NAME, false);
+		$this->db->dropTable(\LiveVoting\Voter\xlvoVoter::TABLE_NAME, false);
 
 		return true;
 	}
