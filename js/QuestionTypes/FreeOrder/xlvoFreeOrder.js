f6a56c932d1e30111cec69f338361bc7498b301a
Florian <fw@studer-raimann.ch>
2018-06-28 15:38:17 +0200

    WiP FreeInput enter submit

--- js/QuestionTypes/FreeOrder/xlvoFreeOrder.js
+++ js/QuestionTypes/FreeOrder/xlvoFreeOrder.js
@@ -3,21 +3,35 @@
  * @type {{}}
  */
 var xlvoFreeOrder = {
-    init: function (json) {
-        var config = JSON.parse(json);
-        var replacer = new RegExp('amp;', 'g');
-        config.base_url = config.base_url.replace(replacer, '');
-        this.config = config;
-        this.ready = true;
-    },
-    config: {},
-    base_url: '',
-    run: function () {
-    },
-    /**
-     * @param button_id
-     * @param button_data
-     */
-    handleButtonPress: function (button_id, button_data) {
-    }
+	init: function (json) {
+		var config = JSON.parse(json);
+		var replacer = new RegExp('amp;', 'g');
+		config.base_url = config.base_url.replace(replacer, '');
+		this.config = config;
+		this.ready = true;
+
+		setTimeout(function () {
+			alert();
+			$('input[name^="vote_multi_line_input["][name$="][free_input]"]').each(function (i, input) {
+				alert();
+				$(input).on("keyup", function (e) {
+					if (e.keyCode === 13) {alert();
+						e.preventDefault();
+
+						input.form.submit();
+					}
+				});
+			});
+		}, 500);
+	},
+	config: {},
+	base_url: '',
+	run: function () {
+	},
+	/**
+	 * @param button_id
+	 * @param button_data
+	 */
+	handleButtonPress: function (button_id, button_data) {
+	}
 };
