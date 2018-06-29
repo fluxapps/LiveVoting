e2eafd3921a2e5d5e87aedaec3d397fa0cc7a952
Florian <fw@studer-raimann.ch>
2018-06-29 09:29:44 +0200

    Fix number range left and right buttons

--- js/QuestionTypes/NumberRange/xlvoNumberRange.js
+++ js/QuestionTypes/NumberRange/xlvoNumberRange.js
@@ -29,24 +29,24 @@ var xlvoNumberRange = {
 		slider.change(
 			function (changedValues) {
 				$('#number-display').text(String(changedValues.value.newValue).concat(thisRef.percentageSign));
-		}).bind(thisRef);
+			}.bind(thisRef));
 
 		//left button click event to move the slider to the left
 		var buttonMoveSliderLeft = document.querySelector("#btn-slider-left");
-		buttonMoveSliderLeft.onclick = function() {
+		buttonMoveSliderLeft.onclick = function () {
 
 			var sliderValue = slider.bootstrapSlider("getValue");
-			if(slider.bootstrapSlider("getAttribute", "min") < sliderValue)
+			if (slider.bootstrapSlider("getAttribute", "min") < sliderValue)
 				slider.bootstrapSlider("setValue", sliderValue - 1, false, true);
 
 		}.bind(slider);
 
 		//right button click event to move the slider to the right
 		var buttonMoveSliderRight = document.querySelector("#btn-slider-right");
-		buttonMoveSliderRight.onclick = function() {
+		buttonMoveSliderRight.onclick = function () {
 
 			var sliderValue = slider.bootstrapSlider("getValue");
-			if(slider.bootstrapSlider("getAttribute", "max") > sliderValue)
+			if (slider.bootstrapSlider("getAttribute", "max") > sliderValue)
 				slider.bootstrapSlider("setValue", sliderValue + 1, false, true);
 
 		}.bind(slider);
