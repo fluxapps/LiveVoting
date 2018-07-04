840fd8c27560ac80b24a67268b538d51049baa95
Martin Studer <ms@studer-raimann.ch>
2018-07-04 14:26:08 +0200

    FIX Default value for parameters with a class type hint can only be NULL

--- classes/Puk/class.xlvoPuk.php
+++ classes/Puk/class.xlvoPuk.php
@@ -12,7 +12,7 @@ class xlvoPuk extends xlvoPin {
 	/**
 	 * @param string $pin
 	 */
-	public function __construct(string $pin = "") {
+	public function __construct($pin = "") {
 		$this->pin_length = 10;
 
 		parent::__construct($pin);
