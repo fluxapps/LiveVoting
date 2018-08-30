09f9b92067c54e4b40e5def08d19ef69c019e345
Florian <fw@studer-raimann.ch>
2018-08-30 10:32:55 +0200

    Fix strange sort

--- src/QuestionTypes/NumberRange/xlvoNumberRangeResultsGUI.php
+++ src/QuestionTypes/NumberRange/xlvoNumberRangeResultsGUI.php
@@ -141,7 +141,7 @@ class xlvoNumberRangeResultsGUI extends xlvoInputResultsGUI {
 	 */
 	private function renderGroupedTextResult() {
 		$bars = new xlvoBarGroupingCollectionGUI();
-		$bars->sorted(true);
+		//$bars->sorted(true);
 		$votes = $this->manager->getVotesOfVoting();
 		usort($votes, function (xlvoVote $v1, xlvoVote $v2) {
 			return (intval($v1->getFreeInput()) - intval($v2->getFreeInput()));
