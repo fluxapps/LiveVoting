bba69f2e44b0bd54f74991421762a26f7520cab8
Florian <fw@studer-raimann.ch>
2018-09-12 07:17:59 +0200

    Fix

--- classes/uninstall/class.LiveVotingRemoveDataConfirm.php
+++ classes/uninstall/class.LiveVotingRemoveDataConfirm.php
@@ -18,14 +18,6 @@ class LiveVotingRemoveDataConfirm extends AbstractRemovePluginDataConfirm {
 	/**
 	 * @inheritdoc
 	 */
-	public function removeUninstallRemovesData() {
-		xlvoConf::remove(self::KEY_UNINSTALL_REMOVES_DATA);
-	}
-
-
-	/**
-	 * @inheritdoc
-	 */
 	public function getUninstallRemovesData() {
 		return xlvoConf::getConfig(self::KEY_UNINSTALL_REMOVES_DATA);
 	}
@@ -36,5 +28,13 @@ class LiveVotingRemoveDataConfirm extends AbstractRemovePluginDataConfirm {
 	 */
 	public function setUninstallRemovesData($uninstall_removes_data) {
 		xlvoConf::set(self::KEY_UNINSTALL_REMOVES_DATA, $uninstall_removes_data);
+	}
+
+
+	/**
+	 * @inheritdoc
+	 */
+	public function removeUninstallRemovesData() {
+		xlvoConf::remove(self::KEY_UNINSTALL_REMOVES_DATA);
 	}
 }
