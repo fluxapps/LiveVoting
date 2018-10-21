d825377580d13e2823ad0d0e40cd7ed8950fb62d
Martin Studer <ms@studer-raimann.ch>
2018-10-21 17:21:23 +0200

    fix rewrite url

--- src/Conf/xlvoConf.php
+++ src/Conf/xlvoConf.php
@@ -153,7 +153,7 @@ class xlvoConf extends CachingActiveRecord {
 	 *
 	 * @deprecated
 	 */
-	const REWRITE_RULE_PRESENTER = "RewriteRule ^presenter(/\\w*)(/\\w*)(/\\w*)?(/\\w*)? Customizing/global/plugins/Services/Repository/RepositoryObject/LiveVoting/presenter.php?".ParamManager::PARAM_PIN."=$1&puk=$2&voting=$3&ppt=$4 [L]";
+	const REWRITE_RULE_PRESENTER = "RewriteRule ^presenter(/\\w*)(/\\w*)(/\\w*)?(/\\w*)? Customizing/global/plugins/Services/Repository/RepositoryObject/LiveVoting/presenter.php?xlvo_pin=$1&puk=$2&voting=$3&ppt=$4 [L]";
 	/**
 	 * Min client update frequency in seconds.
 	 * This value should never be set bellow 1 second.
