63b81a7569a1c51f7178bfbd663dd3a58019a72b
Martin Studer <ms@studer-raimann.ch>
2018-10-24 11:13:49 +0200

    fixed bug request_rule_vote / PARAMMANAGER::XYZ does not work on ikt.ch

--- src/Conf/xlvoConf.php
+++ src/Conf/xlvoConf.php
@@ -81,7 +81,7 @@ class xlvoConf extends CachingActiveRecord {
 	 *
 	 * @deprecated
 	 */
-	const REWRITE_RULE_VOTE = 'RewriteRule ^vote(/[\\w]*|) Customizing/global/plugins/Services/Repository/RepositoryObject/LiveVoting/pin.php?'. ParamManager::PARAM_PIN . '=$1 [L]';
+	const REWRITE_RULE_VOTE = 'RewriteRule ^vote(/[\\w]*|) Customizing/global/plugins/Services/Repository/RepositoryObject/LiveVoting/pin.php?xlvo_pin=$1 [L]';
 	/**
 	 * @var string
 	 *
@@ -153,8 +153,7 @@ class xlvoConf extends CachingActiveRecord {
 	 *
 	 * @deprecated
 	 */
-	const REWRITE_RULE_PRESENTER = "RewriteRule ^presenter(/\\w*)(/\\w*)(/\\w*)?(/\\w*)? Customizing/global/plugins/Services/Repository/RepositoryObject/LiveVoting/presenter.php?"
-	. ParamManager::PARAM_PIN . "=$1&puk=$2&voting=$3&ppt=$4 [L]";
+	const REWRITE_RULE_PRESENTER = "RewriteRule ^presenter(/\\w*)(/\\w*)(/\\w*)?(/\\w*)? Customizing/global/plugins/Services/Repository/RepositoryObject/LiveVoting/presenter.php?xlvo_pin=$1&puk=$2&voting=$3&ppt=$4 [L]";
 	/**
 	 * Min client update frequency in seconds.
 	 * This value should never be set bellow 1 second.
