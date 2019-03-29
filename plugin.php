1b5b00127b52112ca488f4c3a0946016d15262e6
Adrian Lüthi <al@studer-raimann.ch>
2019-03-29 14:16:20 +0100

    update plugin.php for 5.4

--- plugin.php
+++ plugin.php
@@ -5,6 +5,6 @@ require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 $id = 'xlvo';
 $version = '4.1.1';
 $ilias_min_version = '5.2.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag - Team Custom 1';
 $responsible_mail = 'support-custom1@studer-raimann.ch';
