46532057c33cd65dc61f71ef09218b2d2f16d919
Martin Studer <ms@studer-raimann.ch>
2020-07-22 08:45:29 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = 'xlvo';
-$version = '4.4.3';
+$version = '5.0.0';
 $ilias_min_version = '5.4.0';
 $ilias_max_version = '6.999';
 $responsible = 'studer + raimann ag - Team Custom 1';
